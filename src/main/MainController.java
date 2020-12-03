6e18d7c8a698e3b19510b0ace8d7e677061d782a
Moritz Kronberger <moritzkronberger@aon.at>
2020-12-03 19:18:15 +0100

    Making Welcome Screen

--- src/main/MainController.java
+++ src/main/MainController.java
@@ -2,11 +2,33 @@ package main;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+
+    @FXML
+    private TextField answer;
+
+    public void givenUsingPlainJava_whenGeneratingRandomStringUnbounded_thenCorrect() {
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+
+        System.out.println(generatedString);
+    }
+
+    @FXML
+    private void ask()
+    {
+
+    }
+
 
     private String myData = "";
 
--- src/welcome/WelcomeController.java
+++ src/welcome/WelcomeController.java
@@ -5,8 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import main.MainController;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,19 +27,52 @@ public class WelcomeController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    Label l = new Label();
 
     @FXML
     private void next()
     {
-        //navigate from welcome screen to main screen
-        System.out.println("Navigation started ...");
 
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main/main.fxml"));
-            Parent root = fxmlLoader.load();
+        String name = username.getText();
+        String pw = password.getText();
 
-            //send data to MainController
-            main.MainController ctrl = fxmlLoader.getController();
+
+
+    if(name.equals("") && pw.equals(""))
+    {
+        l.setStyle("-fx-text-fill: red;");
+        l.setText("Password and Username missing");
+    }
+
+    else if(name.equals(""))
+    {
+        l.setStyle("-fx-text-fill: red;");
+        l.setText("Username missing");
+    }
+    else if(pw.equals(""))
+    {
+        l.setStyle("-fx-text-fill: red;");
+        l.setText("Password missing");
+    }
+
+    else if(name.equals("user") && pw.equals("password"))
+    {
+
+
+            l.setStyle("-fx-text-fill: green;");
+            l.setText("Correct!!");
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+            Parent root = null;
+            
+        //send data to MainController
+            MainController ctrl = fxmlLoader.getController();
             ctrl.setMyData("Hello from Welcome Controller!");
 
             Stage mainStage = new Stage();
@@ -43,10 +81,17 @@ public class WelcomeController implements Initializable {
             mainStage.show();
 
             this.stage.close();
-        }
-        catch(Exception ex)
-        {
-            System.out.println("Exception occured while loading main view!");
-        }
     }
+
+    else
+    {
+        l.setStyle("-fx-text-fill: red;");
+        l.setText("Wrong Password");
+    }
+
+
+
+
+     }
+
 }
